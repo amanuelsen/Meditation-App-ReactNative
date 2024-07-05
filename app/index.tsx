2b8867e775a4eeaade74d940621d380f6768f090
senai <saniy.cr7@gmail.com>
2024-07-05 15:23:53 +0100

    home page

--- app/index.tsx
+++ app/index.tsx
@@ -1,17 +1,41 @@
-import { Image, StyleSheet, Platform, Text, View } from 'react-native';
+import { View, Text, ImageBackground, TouchableOpacity } from 'react-native';
+import React from 'react';
+import beachImage from "@/assets/meditation-images/beach.webp";
+import { LinearGradient } from "expo-linear-gradient";
+import { StatusBar } from "expo-status-bar";
+import { SafeAreaView } from 'react-native-safe-area-context';
 
-export default function HomeScreen() {
+const App = () => {
   return (
-    <View style={styles.container}>
-      <Text>Hej</Text>
+    <View style={{ flex: 1 }}>
+      <ImageBackground source={beachImage} resizeMode="cover" style={{ flex: 1 }}>
+        <LinearGradient colors={["rgba(0,0,0,0.4)", "rgba(0,0,0,0.8)"]} style={{ flex: 1 }}>
+          <SafeAreaView  style={{ flex: 1, marginHorizontal: 20, marginVertical: 48, justifyContent: 'space-between' }}>
+            <View>
+              <Text style={{ textAlign: 'center', color: 'white', fontWeight: 'bold', fontSize: 32 }}>Simple Meditation</Text>
+              <Text style={{ textAlign: 'center', color: 'white', fontSize: 24, marginTop: 12 }}>Simplifying for everyone</Text>
+            </View>
+            <View>
+              <TouchableOpacity
+                activeOpacity={0.7}
+                style={{
+                  backgroundColor: 'white',
+                  borderRadius: 12,
+                  minHeight: 62,
+                  justifyContent: 'center',
+                  alignItems: 'center'
+                }}
+                onPress={() => console.log("tap")}
+              >
+                <Text style={{ color: 'black', fontWeight: '600', fontSize: 18 }}>Get started</Text>
+              </TouchableOpacity>
+            </View>
+            <StatusBar style="light" />
+          </SafeAreaView>
+        </LinearGradient>
+      </ImageBackground>
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-  },
-});
+export default App;
---
+++ components/CustomeButton.tsx
@@ -0,0 +1,37 @@
+import { View, Text, TouchableOpacity, StyleSheet } from 'react-native';
+import React from 'react';
+
+interface CustomeButtonProps {
+    onPress: () => void;
+    title: string;
+    textStyles?: object;
+    containerStyle?: object;
+}
+
+const CustomeButton = ({ onPress, title, textStyles = {}, containerStyle = {} }: CustomeButtonProps) => {
+    return (
+        <TouchableOpacity
+            activeOpacity={0.7}
+            style={[styles.button, containerStyle]}
+            onPress={onPress}
+        >
+            <Text style={[styles.text, textStyles]}>{title}</Text>
+        </TouchableOpacity>
+    );
+}
+
+const styles = StyleSheet.create({
+    button: {
+        backgroundColor: 'green',
+        borderRadius: 12,
+        minHeight: 62,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    text: {
+        fontWeight: '600',
+        fontSize: 18,
+    },
+});
+
+export default CustomeButton;
