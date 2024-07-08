c8350ba6fa5e673e0bc5814b1a06fd130e282535
senai <saniy.cr7@gmail.com>
2024-07-08 13:34:55 +0100

    Fixing Nature meditation page

--- app/(tabs)/nature-meditate.tsx
+++ app/(tabs)/nature-meditate.tsx
@@ -1,12 +1,98 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { LinearGradient } from "expo-linear-gradient";
+import { StatusBar } from "expo-status-bar";
+import { router } from "expo-router";
+import React from "react";
+import {
+    FlatList,
+    ImageBackground,
+    Pressable,
+    ScrollView,
+    Text,
+    View,
+} from "react-native";
 
-const NatureMeditate = () => {
-  return (
-    <View>
-      <Text>NatureMeditate</Text>
-    </View>
-  )
-}
+import MEDITATION_IMAGES from "@/constants/Meditation-images";
+import { MEDITATION_DATA } from "@/constants/MeditationData";
+import AppGradient from "@/components/AppGradient";
 
-export default NatureMeditate
+const NatureMeditation = () => {
+    return (
+        <ScrollView contentContainerStyle={{ flexGrow: 1 }}>
+            <View style={{ flex: 1 }}>
+                <AppGradient
+                    colors={["#161b2e", "#0a4d4a", "#766e67"]}
+                >
+                    <View style={{ marginBottom: 24 }}>
+                        <Text style={{
+                            color: '#E5E7EB',
+                            marginBottom: 12,
+                            fontWeight: 'bold',
+                            fontSize: 32,
+                            textAlign: 'left'
+                        }}>
+                            Welcome Steven
+                        </Text>
+                        <Text style={{
+                            color: '#C7D2FE',
+                            fontSize: 20,
+                            fontWeight: '500'
+                        }}>
+                            Start your meditation practice today
+                        </Text>
+                    </View>
+                    <View>
+                        <FlatList
+                            data={MEDITATION_DATA}
+                            contentContainerStyle={{ paddingBottom: 150 }}
+                            keyExtractor={(item) => item.id.toString()}
+                            showsVerticalScrollIndicator={false}
+                            renderItem={({ item }) => (
+                                <Pressable
+                                    onPress={() => router.push(`/meditate/${item.id}`)}
+                                    style={{
+                                        height: 192,
+                                        marginVertical: 12,
+                                        borderRadius: 8,
+                                        overflow: 'hidden'
+                                    }}
+                                >
+                                    <ImageBackground
+                                        source={MEDITATION_IMAGES[item.id - 1]}
+                                        resizeMode="cover"
+                                        style={{
+                                            flex: 1,
+                                            borderRadius: 10,
+                                            justifyContent: 'center'
+                                        }}
+                                    >
+                                        <LinearGradient
+                                            colors={["transparent", "rgba(0,0,0,0.8)"]}
+                                            style={{
+                                                alignItems: 'center',
+                                                height: '100%',
+                                                justifyContent: 'center',
+                                                width: '100%'
+                                            }}
+                                        >
+                                            <Text  style={{
+                                                color: '#F3F4F6',
+                                                fontSize: 24,
+                                                fontWeight: 'bold',
+                                                textAlign: 'center'
+                                            }}>
+                                                {item.title}
+                                            </Text>
+                                        </LinearGradient>
+                                    </ImageBackground>
+                                </Pressable>
+                            )}
+                        />
+                    </View>
+                </AppGradient>
+                <StatusBar style="light" />
+            </View>
+        </ScrollView>
+    );
+};
+
+export default NatureMeditation;
--- app/index.tsx
+++ app/index.tsx
@@ -6,33 +6,30 @@ import { StatusBar } from "expo-status-bar";
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { useRouter } from "expo-router";
 import CustomeButton from '@/components/CustomeButton';
-import AppGradient from '@/components/AppGradient';
 const App = () => {
   const router=useRouter();
   return (
     <View style={{ flex: 1 }}>
       <ImageBackground source={beachImage} resizeMode="cover" style={{ flex: 1 }}>
-
-        <AppGradient colors={["rgba(0,0,0,0.4)", "rgba(0,0,0,0.8)"]} >
-          <LinearGradient colors={["rgba(0,0,0,0.4)", "rgba(0,0,0,0.8)"]} style={{ flex: 1 }}>
-            <SafeAreaView  style={{ flex: 1, marginHorizontal: 4, marginVertical: 48, justifyContent: 'space-between' }}>
-              <View>
-                <Text style={{ textAlign: 'center', color: 'white', fontWeight: 'bold', fontSize: 32 }}>Simple Meditation</Text>
-                <Text style={{ textAlign: 'center', color: 'white', fontSize: 24, marginTop: 12 }}>Simplifying for everyone</Text>
-              </View>
-              <View>
-                <CustomeButton
-                  title='Get Started'
-          
-                  onPress={() =>router.push("/nature-meditate") }
-                >
-                  <Text style={{ color: 'black', fontWeight: '600', fontSize: 18 }}>Get started</Text>
-                </CustomeButton>
-              </View>
-              <StatusBar style="light" />
-            </SafeAreaView>
-          </LinearGradient>
-        </AppGradient>
+        
+        <LinearGradient colors={["rgba(0,0,0,0.4)", "rgba(0,0,0,0.8)"]} style={{ flex: 1 }}>
+          <SafeAreaView  style={{ flex: 1, marginHorizontal: 20, marginVertical: 48, justifyContent: 'space-between' }}>
+            <View>
+              <Text style={{ textAlign: 'center', color: 'white', fontWeight: 'bold', fontSize: 32 }}>Simple Meditation</Text>
+              <Text style={{ textAlign: 'center', color: 'white', fontSize: 24, marginTop: 12 }}>Simplifying for everyone</Text>
+            </View>
+            <View>
+              <CustomeButton
+                title='Get Started'
+                
+                onPress={() =>router.push("/nature-meditate") }
+              >
+                <Text style={{ color: 'black', fontWeight: '600', fontSize: 18 }}>Get started</Text>
+              </CustomeButton>
+            </View>
+            <StatusBar style="light" />
+          </SafeAreaView>
+        </LinearGradient>
       </ImageBackground>
     </View>
   );
