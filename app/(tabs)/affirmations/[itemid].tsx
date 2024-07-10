51801c57ec6365de22a278f16c4d081065d212d8
senai <saniy.cr7@gmail.com>
2024-07-10 20:12:23 +0100

    styling affirmations pages

--- app/(tabs)/affirmations/[itemid].tsx
+++ app/(tabs)/affirmations/[itemid].tsx
@@ -1,9 +1,11 @@
-import { View, Text } from 'react-native';
+import { View, Text, ImageBackground, Pressable } from 'react-native';
 import React, { useEffect, useState } from 'react';
-import { useLocalSearchParams } from 'expo-router';
+import { useLocalSearchParams, router } from 'expo-router';
 import { GalleryPreviewData } from '@/components/models/AffirmationsCategory';
 import AFFIRMATION_GALLERY from '@/constants/affirmation-gallery';
-
+import AppGradient from '@/components/AppGradient';
+import { AntDesign } from '@expo/vector-icons';
+import { ScrollView } from 'react-native-gesture-handler';
 const AffirmationPractice = () => {
     const { itemid } = useLocalSearchParams();
     const [affirmation, setAffirmation] = useState<GalleryPreviewData | null>(null);
@@ -19,17 +21,25 @@ const AffirmationPractice = () => {
 
         if (foundAffirmation) {
             setAffirmation(foundAffirmation);
+            const affirmationArray= foundAffirmation.text.split(",");
+            if(affirmationArray[affirmationArray.length-1]===""){
+                affirmationArray.pop();
+            }
         }
     }, [itemid]);
 
     return (
         <View style={{ flex: 1 }}>
-            <Text>Affirmation Practice</Text>
-            {affirmation && (
-                <View>
-                    <Text>{affirmation.text}</Text>
-                </View>
-            )}
+            <ImageBackground source={affirmation?.image} resizeMode='cover' style={{flex:1}}>
+                <AppGradient colors={["rgba(0,0,0.3)", "rgba(0,0,0,0.9)"]}>
+                    <Pressable style={{position:'absolute', top:16, left:6, zIndex:10}} onPress={()=>router.back()}><AntDesign name="leftcircleo" size={50} color="white" /></Pressable>
+                    <ScrollView style={{marginTop:20}} showsVerticalScrollIndicator={false}>
+                        <View style={{height:100, justifyContent:'center'}}>
+                            <Text style={{color:"white", fontSize:300,marginBottom:12, fontWeight:"bold", textAlign:"center" }}>                            {affirmation?.text}
+                            </Text>
+                        </View></ScrollView>
+                </AppGradient>
+            </ImageBackground>
         </View>
     );
 };
